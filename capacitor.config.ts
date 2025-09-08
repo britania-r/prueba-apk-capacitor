430dfa8465c4c8f365b9d839403e9ec5fc0b604b
Mister H <171655072+britania-r@users.noreply.github.com>
2025-09-08 00:35:26 -0500

    Configure Capacitor and add Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'prueba-apk-capacitor',
+  webDir: 'build'
+};
+
+export default config;
